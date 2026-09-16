a68f70b1976bb98d3916de52dd3e083cfd30aa09
zhengzishan <zhengzishan@u.nus.edu>
2026-09-16 13:52:18 +0800

    zzs test: remove unused import

--- backend/src/test/java/sg/nus/carelink/incident/controller/EmergencyCallControllerTest.java
+++ backend/src/test/java/sg/nus/carelink/incident/controller/EmergencyCallControllerTest.java
@@ -1,7 +1,6 @@
 package sg.nus.carelink.incident.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
